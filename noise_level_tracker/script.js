ae42516318cab08c6a87cddae0c066413b0d9c34
AnkitSingh-16 <98884735+AnkitSingh-16@users.noreply.github.com>
2023-05-12 10:15:25 +0530

    Update script.js

--- noise_level_tracker/script.js
+++ noise_level_tracker/script.js
@@ -81,18 +81,3 @@ function changeUpdateRate() {
 }
 
 
-// update the date of last project's version
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    let repos = JSON.parse(this.responseText);
-    repos.forEach((repo)=>{
-        if (repo.name == "db-meter") {
-            date = new Date(repo.pushed_at);
-            document.getElementById("date").innerText = date.toLocaleDateString();
-        }
-    });
-  }
-};
-
-xhttp.send();
